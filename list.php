04a22a42dc1a916b3261ecfc4d990a2a1dfbc1c6
walker-gibbons <35929732+walker-gibbons@users.noreply.github.com>
2021-03-29 16:25:03 -0400

    Update list.php

--- list.php
+++ list.php
@@ -3,11 +3,11 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <body style="background:rgb(255,129,0,0.5)">
 <div>
-<button onclick="document.location='/mapproj'">Home</button>
-<button onclick="document.location='/mapproj/map.php'">Interactive Map</button>
-<button onclick="document.location='/mapproj/list.php'">Full List of Programs</button>
-<button onclick="document.location='/mapproj/scholarships.php'">Scholarship Opportunities</button>
-<button onclick="document.location='/mapproj/contact.php'">Contact Us!</button>
+<button onclick="document.location='/seniorpoject'">Home</button>
+<button onclick="document.location='/seniorproject/map.php'">Interactive Map</button>
+<button onclick="document.location='/seniorproject/list.php'">Full List of Programs</button>
+<button onclick="document.location='/seniorproject/scholarships.php'">Scholarship Opportunities</button>
+<button onclick="document.location='/seniorproject/contact.php'">Contact Us!</button>
 </div>
 <br>
 <h1 style="border-left:solid 6px;border-color:red;">Program List Page Test</h1>
